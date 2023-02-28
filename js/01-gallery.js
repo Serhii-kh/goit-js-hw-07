df03fb564ff183f8793dc09b7258e4c97105b351
Serhii Naboka <drifterzzz@gmail.com>
2023-02-28 17:24:42 +0200

    gfgfg

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryDivRef = document.querySelector(".gallery");
+console.log(galleryDivRef);
+
+const createGalleryCardsMarkup = (galleryItems) => {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      `
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="large-image.jpg"
+      alt="${description}"
+    />
+  </a>
+</div>
+`;
+    })
+    .join("");
+};
+
+const galleryMarkup = createGalleryCardsMarkup(galleryItems);
+
+galleryDivRef.insertAdjacentHTML("beforeend", galleryMarkup);
+
+console.log(galleryDivRef);
