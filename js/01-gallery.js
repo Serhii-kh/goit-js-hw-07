47ef483c77996db5d8a6e536d0639d7b4f2b240d
Serhii Naboka <drifterzzz@gmail.com>
2023-03-01 20:25:29 +0200

    asas
    markup done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,29 +1,34 @@
 import { galleryItems } from "./gallery-items.js";
 
 const galleryDivRef = document.querySelector(".gallery");
-console.log(galleryDivRef);
 
-const createGalleryCardsMarkup = (galleryItems) => {
-  return galleryItems
+const createGalleryMarkup = (items) => {
+  return items
     .map(({ preview, original, description }) => {
-      `
-    <div class="gallery__item">
+      return `<div class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
       src="${preview}"
-      data-source="large-image.jpg"
+      data-source="${original}"
       alt="${description}"
     />
   </a>
 </div>
+
 `;
     })
     .join("");
 };
 
-const galleryMarkup = createGalleryCardsMarkup(galleryItems);
+const galleryMarkup = createGalleryMarkup(galleryItems);
 
 galleryDivRef.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryDivRef.addEventListener("click", onGalleryDivClick);
+
+function onGalleryDivClick(e) {
+  console.log(e.target);
+}
 
 console.log(galleryDivRef);
