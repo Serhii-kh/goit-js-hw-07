70bbe5e87abb57bbfef75b0df32d5034b9ebf5d7
Serhii Naboka <drifterzzz@gmail.com>
2023-03-03 12:10:21 +0200

    gffg

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryDivRef = document.querySelector(".gallery");
+
+const createGalleryMarkup = (items) => {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</div>
+`;
+    })
+    .join("");
+};
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryDivRef.insertAdjacentHTML("beforeend", galleryMarkup);
+
+
